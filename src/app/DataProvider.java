c04d6743c4c48dbf6db2d903e5b07c05bd7903f0
Marys888 <severovamaha1999@gmail.com>
2025-04-26 13:10:27 +0200

    Add data provider implementation

---
+++ src/app/DataProvider.java
@@ -0,0 +1,9 @@
+package app;
+
+public class DataProvider {
+    public String[] getData() {
+        return new String[]{
+                "Orange", "plum", "tomato", "onibn","grape"
+        };
+    }
+}
