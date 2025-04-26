512123e9f1e2ef223fb4c7cf82fa7753485354e7
Marys888 <severovamaha1999@gmail.com>
2025-04-26 13:58:11 +0200

    Add entry point logic in Main class to connect DataProvider and Corrector

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,15 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        DataProvider dataProvider = new DataProvider();
+        Correcter correcter = new Correcter();
+        
+        String[] data = dataProvider.getData();
+        String correctedData = correcter.handleData(data);
 
+        getOutput(correctedData);
+    }
+    public static void getOutput(String output) {
+        System.out.println(output);
     }
 }
